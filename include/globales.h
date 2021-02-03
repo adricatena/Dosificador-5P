539a83db562d6e030a47c95eb591bad665ad82af
adricatena <adricatena@gmail.com>
2021-02-03 17:21:42 -0300

    define bomba, ev1 y balanzas

--- include/globales.h
+++ include/globales.h
@@ -1,16 +1,26 @@
+#include <HX711.h>
+
 #define encoderPinA 2
 #define encoderPinB 3
 #define encoderBoton 4
+#define BTN_verde 14
+#define BTN_negro 15
 #define DT1 22  // Celda 1
 #define SCK1 23
 #define DT2 24  // Celda 2
 #define SCK2 25
 #define DT3 26  // Celda 3
-#define SCK1 27
+#define SCK3 27
 #define DT4 28  // Celda 4
-#define SCK1 29
+#define SCK4 29
 #define DT5 30  // Celda 5
 #define SCK5 31
+#define BOMBA 32
+#define EV1 33
+#define LUZ1r 34
+#define LUZ2v 35
+
+HX711 balanza1, balanza2, balanza3, balanza4, balanza5;
 
 unsigned int flag = 0;
 unsigned int seleccion = 1; // Seleccion de item
